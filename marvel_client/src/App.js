eff1ebc520d67c0d836e0903c12c801bea7720e9
Chad Jemmett <chad.jemmett@gmail.com>
2018-12-19 23:00:41 -0700

    Fixed the things, hid the secrets

--- marvel_client/src/App.js
+++ marvel_client/src/App.js
@@ -1,27 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import md5 from 'md5'
+import {publicKey, privateKey} from './secret.js'
 
 class App extends Component {
+  constructor() {
+    super()
+      this.state = {
+        comics: [],
+        opts: 'series/1987',
+      }
+  }
+
+  componentDidMount() {
+    const ts = Date.now()
+    const hash = md5(ts + privateKey + publicKey)
+    axios.get(`https://gateway.marvel.com:443/v1/public/${this.state.opts}?apikey=${publicKey}&hash=${hash}&ts=${ts}`)
+    .then(res => {
+      this.setState({comics: res.data.data.results[0].comics.items})
+    })
+    .catch(err => console.log(err))
+
+  }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    if(this.state.comics.length > 0) {
+      return (
+          <div>
+          {this.state.comics.map(item => (
+            <p>{item.name}</p>
+          ))}
+          </div>
+          )
+    } else {
+      return (
+          <div>Getting comics</div>
+          )
+    }
   }
 }
 
